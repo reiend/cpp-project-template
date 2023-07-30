57714c1b68a1d4a2e961212d9da68444f476bf74
reiend <loncerasjakerei@gmail.com>
2023-07-30 22:52:58 +0800

    initial editor configuration

---
+++ lua/user/reiend/colorschemes.lua
@@ -0,0 +1,9 @@
+function setColorSchemeEverforest()
+  -- value = hard | medium | soft
+  vim.g.everforest_background = "hard"
+  vim.g.everforest_better_performance = 1
+  vim.opt.background = "dark"
+  vim.cmd([[ colorscheme everforest ]])
+end
+
+setColorSchemeEverforest()
---
+++ lua/user/reiend/init.lua
@@ -0,0 +1,5 @@
+require "user.reiend.options"
+require "user.reiend.plugins"
+require "user.reiend.colorschemes"
+require "user.reiend.lualine"
+require "user.reiend.nvim-treesitter"
