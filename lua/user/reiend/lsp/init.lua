bfcb9ecf03ee7dcee7bd2a7a933ea5875f72d59e
reiend <loncerasjakerei@gmail.com>
2023-07-31 00:12:30 +0800

    added keymap for formatting project

--- lua/user/reiend/lsp/init.lua
+++ lua/user/reiend/lsp/init.lua
@@ -3,3 +3,11 @@ require("mason-lspconfig").setup()
 
 -- sarvers
 require "user.reiend.lsp.servers.lua_ls"
+
+-- format
+local keymap = vim.keymap.set
+keymap('n', '<leader>pf', function(_)
+  vim.lsp.buf.format()
+end, { desc = '[P]roject [F]ormat' })
+
+vim.api.nvim_command("command! Format lua vim.lsp.buf.format()")
