7e3f5307d628350b4413da96f8aa737709f9f94e
reiend <loncerasjakerei@gmail.com>
2023-07-30 23:54:00 +0800

    added plugin for searching file directory

--- lua/user/reiend/plugins.lua
+++ lua/user/reiend/plugins.lua
@@ -39,4 +39,10 @@ require("lazy").setup({
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
+
+  -- code completion
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 })
