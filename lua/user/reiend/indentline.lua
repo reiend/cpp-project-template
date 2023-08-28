d223cbaef602858a8386911bc35a831087551055
reiend <loncerasjakerei@gmail.com>
2023-08-29 01:55:41 +0800

    indentline configs

---
+++ lua/user/reiend/indentline.lua
@@ -0,0 +1,37 @@
+local status_ok, indent_blankline = pcall(require, 'indent_blankline')
+if not status_ok then
+  return
+end
+
+vim.g.indent_blankline_filetype_exclude = {
+  'help',
+  'startify',
+  'dashboard',
+  'packer',
+  'neogitstatus',
+  'NvimTree',
+  'Netrw',
+  'Trouble',
+}
+
+vim.g.indentLine_enabled = 1
+vim.g.indent_blankline_char = "│"
+-- vim.g.indent_blankline_char = '▏'
+-- vim.g.indent_blankline_char = "▎"
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_show_current_context = true
+
+-- issue #59
+vim.wo.colorcolumn = '99999'
+
+vim.opt.list = true
+-- vim.opt.listchars:append 'eol:↴'
+vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
+
+indent_blankline.setup {
+  show_current_context = true,
+  show_current_context_start = true,
+}
+
