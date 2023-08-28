1f6392941208a9df7982a28e4236bf1605a2b80a
reiend <loncerasjakerei@gmail.com>
2023-08-29 01:57:05 +0800

    updated treesitter configs

--- lua/user/reiend/nvim-treesitter.lua
+++ lua/user/reiend/nvim-treesitter.lua
@@ -5,7 +5,14 @@ require('nvim-treesitter.configs').setup {
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
+  default_mappings = true, -- disable buffer local mapping created by this plugin
+  default_commands = true, -- disable commands created by this plugin
+  disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+  list_opener = 'copen', -- command or function to open the conflicts list
+  highlights = { -- They must have background color, otherwise the default color will be used
+    incoming = 'DiffAdd',
+    current = 'DiffText',
+  }, -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
@@ -47,3 +54,4 @@ require('nvim-treesitter.configs').setup {
     enable_close_on_slash = false,
   },
 }
+
